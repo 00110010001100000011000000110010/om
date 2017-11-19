c345ad9fc1f809e20557756e2a5f06a0b4af32bb
Drew <kolesar.andris@gmail.com>
2017-11-19 12:47:48 +0100

    CDV számításának javítása
    #3

--- ConsoleApplication2/cdv checker.cs
+++ ConsoleApplication2/cdv checker.cs
@@ -11,12 +11,12 @@ namespace om
         static void Main(string[] args)
         {string om;
             Console.WriteLine("Kérem írja be az OM azonosítót!");
-            om = Console.ReadLine();
+            om =  Console.ReadLine();
             if (om.Length == 11) Console.WriteLine("Azonosító ellenőrzése...");
             else Console.WriteLine("Rossz azonosító");
             if (om[0] != '7' ) Console.WriteLine("nope");
             else Console.WriteLine("ok");
-            int cdv = (((int)om[1] - 48) * 2 + ((int)om[2] - 48)* 2 + ((int)om[3] - 48) * 2 + ((int)om[4] - 48) * 2 + ((int)om[5] - 48) * 2 + ((int)om[6] - 48) * 2 + ((int)om[7] - 48) * 2 + ((int)om[8] - 48) * 2 + ((int)om[9] - 48) * 2) %11;
+            int cdv = (((int)om[0] - 48) * 1 + ((int)om[1] - 48) * 2 + ((int)om[2] - 48)* 3 + ((int)om[3] - 48) * 4 + ((int)om[4] - 48) * 5 + ((int)om[5] - 48) * 6 + ((int)om[6] - 48) * 7 + ((int)om[7] - 48) *8 + ((int)om[8] - 48) * 9 + ((int)om[9] - 48) * 10) %11;
             Console.WriteLine(cdv);
             if (cdv != (int) om[10] - 48) Console.WriteLine("Rossz azonosító");
             else Console.WriteLine("Helyes");
