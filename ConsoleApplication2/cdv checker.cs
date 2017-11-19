7f6c7fcdad694f5ad338aa8ea9d22db121f5e1ab
Drew <kolesar.andris@gmail.com>
2017-11-19 15:59:18 +0100

    Hibák kezelése kivétellel

--- ConsoleApplication2/cdv checker.cs
+++ ConsoleApplication2/cdv checker.cs
@@ -9,24 +9,32 @@ namespace om
     class Program
     {
         static void Main(string[] args)
-        {string om;
-            Console.WriteLine("Kérem írja be az OM azonosítót!");
-            om = Console.ReadLine();
-            if (om.Length == 11) Console.WriteLine("Azonosító ellenőrzése...");
-            else Console.WriteLine("Rossz azonosító");
-            if (om[0] != '7' ) Console.WriteLine("nope");
-            else Console.WriteLine("ok");
-            int sum = 0;
-            for (int i=0; i<10; i++) {
-                int digit = (int) om[i] - 48;
-                if(digit <0 || digit >9) Console.WriteLine("Rossz számjegy");
-                
-                sum+= digit * (i+1); 
-            }
-            int cdv = sum % 11;
+        {
+            try
+            {
+                string om;
+                Console.WriteLine("Kérem írja be az OM azonosítót!");
+                om = Console.ReadLine();
+                if (om.Length != 11) throw new ArgumentException("Rossz azonosító");
+                if (om[0] != '7') throw new ArgumentException("Hibás az első számjegy");
+                                
+                int sum = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    int digit = (int)om[i] - 48;
+                    if (digit < 0 || digit > 9) throw new ArgumentException("Rossz számjegy");
 
-            if (cdv != (int) om[10] - 48) Console.WriteLine("Rossz azonosító");
-            else Console.WriteLine("Helyes");
+                    sum += digit * (i + 1);
+                }
+                int cdv = sum % 11;
+
+                if (cdv != (int)om[10] - 48) throw new ArgumentException("Rossz azonosító");
+                else Console.WriteLine("Helyes");
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
             
 
             Console.ReadKey();
