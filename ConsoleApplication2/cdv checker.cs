ae36ee83e17f2c2b323b8dfebeead4c9af6e0950
Drew <kolesar.andris@gmail.com>
2017-11-19 16:04:23 +0100

    Ellenőrzés kiszervezése függvénybe

--- ConsoleApplication2/cdv checker.cs
+++ ConsoleApplication2/cdv checker.cs
@@ -15,21 +15,8 @@ namespace om
                 string om;
                 Console.WriteLine("Kérem írja be az OM azonosítót!");
                 om = Console.ReadLine();
-                if (om.Length != 11) throw new ArgumentException("Rossz azonosító");
-                if (om[0] != '7') throw new ArgumentException("Hibás az első számjegy");
-                                
-                int sum = 0;
-                for (int i = 0; i < 10; i++)
-                {
-                    int digit = (int)om[i] - 48;
-                    if (digit < 0 || digit > 9) throw new ArgumentException("Rossz számjegy");
-
-                    sum += digit * (i + 1);
-                }
-                int cdv = sum % 11;
-
-                if (cdv != (int)om[10] - 48) throw new ArgumentException("Rossz azonosító");
-                else Console.WriteLine("Helyes");
+                check(om);
+                Console.WriteLine("Helyes");
             }
             catch (ArgumentException exception)
             {
@@ -39,6 +26,24 @@ namespace om
 
             Console.ReadKey();
 
+        }
+        static void check(string om)
+        {
+
+            if (om.Length != 11) throw new ArgumentException("Rossz azonosító");
+            if (om[0] != '7') throw new ArgumentException("Hibás az első számjegy");
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int digit = (int)om[i] - 48;
+                if (digit < 0 || digit > 9) throw new ArgumentException("Rossz számjegy");
+
+                sum += digit * (i + 1);
+            }
+            int cdv = sum % 11;
+
+            if (cdv != (int)om[10] - 48) throw new ArgumentException("Rossz azonosító");
         }
     }
 }
